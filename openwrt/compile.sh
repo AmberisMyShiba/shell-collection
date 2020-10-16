1e15ffc4b19cebe4e8be413651088a6efb114188
TeF <tefiszx@gmail.com>
2020-10-16 17:56:29 +0800

    Update compile.sh

--- openwrt/compile.sh
+++ openwrt/compile.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+
 git pull
-#./scripts/feeds clean
+./scripts/feeds clean
 ./scripts/feeds update -a && ./scripts/feeds install -a
+#make menuconfig # set target 
+#scripts/diffconfig.sh > mydiffconfig #(save your changes in the text file mydiffconfig).
 make clean 
-make menuconfig # set target 
+cp x86_64_diff.config .config
 make defconfig #to set default config for build system and device
-scripts/diffconfig.sh > mydiffconfig #(save your changes in the text file mydiffconfig).
 make -j$(($(nproc)+1)) || make V=s 2>&1 | tee build.log | grep -i '[^_-"a-z]error[^_-.a-z]' 
+

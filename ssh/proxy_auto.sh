7a2fccd032aa5ea336920c594417e332aea38928
TeF <tefiszx@gmail.com>
2020-11-09 20:27:34 +0800

    Create proxy_auto.sh

---
+++ ssh/proxy_auto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#when ssh login,test status of 3 services which are caddy v2ray and naiveproxy
+#if 3services is not running,start them
+#Date:Mon Nov 9 19  ^z50
+
+CADDY_STATUS1=$(systemctl status caddy |grep Active |awk '{print $3}'|cut -d "(" -f2 |cut -d ")" -f1)
+if [ "CADDY_STATUS" == "dead" ];then
+    echo "Caddy service is not running,It will be started Now!"
+    systemctl start {caddy,v2ray,naiveproxy};else
+    CADDY_STATUS2=$(systemctl status caddy |grep " - " |awk '{print $4,$5,$6,$7}')
+    echo $CADDY_STATUS2" is "$CADDY_STATUS1
+fi

9ea71505426cf760f791693ad545c1b8cece05e7
TeF <tefiszx@gmail.com>
2020-11-09 20:41:19 +0800

    Update proxy_auto.sh

--- ssh/proxy_auto.sh
+++ ssh/proxy_auto.sh
@@ -2,6 +2,7 @@
 #when ssh login,test status of 3 services which are caddy v2ray and naiveproxy
 #if 3services is not running,start them
 #Date:Mon Nov 9 19  ^z50
+#add this shell to lastline in .bashrc
 
 CADDY_STATUS1=$(systemctl status caddy |grep Active |awk '{print $3}'|cut -d "(" -f2 |cut -d ")" -f1)
 if [ "CADDY_STATUS1" == "dead" ];then

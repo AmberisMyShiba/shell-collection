f636691ad7fa7320efdb058f2afc0b31f5e7dec0
TeF <tefiszx@gmail.com>
2020-11-10 14:23:29 +0800

    Create color_echo.sh

---
+++ Functions/color_echo.sh
@@ -0,0 +1,32 @@
+##echo -e "\033[字背景颜色;字体颜色m字符串\033[控制码"
+## blue to echo 
+function blue(){
+    echo -e "\033[34m[ $1 ]\033[0m"
+}
+
+
+## green to echo 
+function green(){
+    echo -e "\033[32m[ $1 ]\033[0m"
+}
+
+## Error to warning with blink
+function bred(){
+    echo -e "\033[31m\033[01m\033[05m[ $1 ]\033[0m"
+}
+
+## Error to warning with blink
+function byellow(){
+    echo -e "\033[33m\033[01m\033[05m[ $1 ]\033[0m"
+}
+
+
+## Error
+function bred(){
+    echo -e "\033[31m\033[01m[ $1 ]\033[0m"
+}
+
+## warning
+function byellow(){
+    echo -e "\033[33m\033[01m[ $1 ]\033[0m"
+}

87f8b5275cc1b85a8e134d839c10e5499c81fb6e
TeF <tefiszx@gmail.com>
2020-12-14 19:34:40 +0800

    Update i3-layout.sh

--- WM&DE/i3-layout.sh
+++ WM&DE/i3-layout.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 i3-msg "workspace 2:fbro; [workspace="__focused__"] kill"
-i3-msg "workspace 2:Fbro; append_layout /home/tef/.i3/workspace-2.json"
+#i3-msg "workspace 2:Fbro; append_layout /home/tef/.i3/workspace-2.json"
+#i3-msg "workspace 2:Fbro; exec gedit; exec urxvt; focus parent; splitv; exec nautilus"
+sleep 0.5
+i3-msg 'workspace 2:Fbro; exec gedit;workspace 1:Comb'
+sleep 0.5
+i3-msg 'workspace 2:Fbro; exec urxvt; workspace 1:Comb'
+sleep 0.5
+i3-msg 'workspace 2:Fbro; focus parent; splitv; exec nautilus'

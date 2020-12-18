0d97dc4d3e3ee6cd5e764b19ed6a9bc856cb84f0
TeF <tefiszx@gmail.com>
2020-12-18 19:47:41 +0800

    Update i3-layout.sh

--- WM&DE/i3-layout.sh
+++ WM&DE/i3-layout.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 # this script can auto load ws2 for vertical of layout
+# start ws2 layout
 i3-msg "workspace 2:fbro; [workspace="__focused__"] kill"
-#i3-msg "workspace 2:Fbro; append_layout /home/tef/.i3/workspace-2.json"
-#i3-msg "workspace 2:Fbro; exec gedit; exec urxvt; focus parent; splitv; exec nautilus"
+####
+####
 sleep 0.5
-i3-msg 'workspace 2:Fbro; exec gedit;workspace 1:Comb'
+i3-msg 'workspace 2:Fbro; exec gedit; workspace 1:Comb'
 sleep 0.5
 i3-msg 'workspace 2:Fbro; exec urxvt; workspace 1:Comb'
 sleep 0.5
 i3-msg 'workspace 2:Fbro; focus parent; splitv; exec nautilus'
+
+#start ws3 layout
+sleep 0.5
+i3-msg 'workspace 3:Term; [workspace="__focused__"] kill'
+sleep 0.5
+i3-msg 'workspace 3:Term; exec urxvt'
+sleep 0.5
+i3-msg 'workspace 3:Term; splitv; exec urxvt; workspace 1:Comb'
+#####
+#####
+sleep 0.5
+i3-msg 'workspace 3:Term; focus parent; splith;exec urxvt'
+sleep 0.5
+i3-msg 'workspace 1:Comb; splith'

0887e8670d7258dfa82cc174b6f70af7c1b240f4
TeF <tefiszx@gmail.com>
2020-12-18 20:02:13 +0800

    Update compile.sh

--- openwrt/compile.sh
+++ openwrt/compile.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 echo -e "pull sources from repository"
-git pull
+#git pull
 echo -e
 echo -e "update and install all feeds"
 echo -e 
-./scripts/feeds update -a
-./scripts/feeds install -a
+#./scripts/feeds update -a
+#./scripts/feeds install -a
 echo -e
 echo -e "prepearing for make compile"
-make clean
-make defconfig
+#make clean
+#make defconfig
 
 #echo -n "Do you want to menuconfig?(y/n)"
-read -s -t10 -p "Do you wanna menuconfig?(y/n)" ac
-if [ $ac = "y" ]; then
-    make menuconfig
-fi
+read -p "Do you wanna menuconfig?(y/n)" ac
+    case $ac in
+        y|Y)
+	make menuconfig;;
+        *)
+        exit 0
+    esac
 echo -e "Downloading feeds and sources"
-make download V=sc
-make -j$(($(nproc)+1)) || make V=s 2>&1 | tee build.log | fgrep -i '[^_-"a-z]error[^_-.a-z]' 
+#echo -n "Do you want to Compile now?(y/n)"
+read -p "Do you want to Compile now?(y/n)" ac
+    case $ac in
+        y|Y)
+        make download V=sc;;
+        *)
+        exit 0
+    esac

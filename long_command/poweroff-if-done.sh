9323e25e4bdbbef5eb56a529e980112cda5a9ff9
TeF <tefiszx@gmail.com>
2021-05-21 19:58:57 +0800

    Update poweroff-if-done.sh

--- long_command/poweroff-if-done.sh
+++ long_command/poweroff-if-done.sh
@@ -1,23 +1,22 @@
-```
 #!/bin/bash
 
 if [[ $# -eq q ]]; then
 # poweroff in 2 minitues
     ps -eo pid | grep $1 &> /dev/null || /sbin/shutdown -h +2
 fi
-```
+
 ----------------------------------------------------------------
-$ `su -c 'while [[ -d /proc/7338 ]]; do sleep 10; done; poweroff'`
+$ su -c 'while [[ -d /proc/7338 ]]; do sleep 10; done; poweroff'
 replace 7338 as task pid
 ----------------------------------------------------------------
 
 
 ctrl+z pause the process,and then type
-`root # bg && wait && poweroff`
+root # bg && wait && poweroff
 or
-`fg; poweroff`
+fg; poweroff
 or
-`fg && poweroff`
+fg && poweroff
 1.bg resumes executing of emerge in the background. 
 2.wait waits for last command sent to background to terminate. 
 3.When emerge finishes with success, poweroff command will be executed. 

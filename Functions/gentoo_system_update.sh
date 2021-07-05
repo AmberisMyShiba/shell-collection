74c645a23838e429e43fd78fe2c3b84499408846
TeF <tefiszx@gmail.com>
2021-07-05 22:00:35 +0800

    Create gentoo_system_update.sh

---
+++ Functions/gentoo_system_update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+intertube=0
+while [ $intertube -ne 1 ]; do
+  ping -c1 www.baidu.com > /dev/null
+  if [ $? -eq 0 ]
+  then
+    sudo layman -S
+    sudo emerge --sync
+    sudo emerge -1uv portage
+    sudo emerge -uDNv --with-bdeps=y --keep-going @world
+    sudo emerge -ca
+    intertube=1
+  else 
+    echo "Waiting for Network connetcion"  && sleep 2
+  fi
+done
+echo "System is updated!"

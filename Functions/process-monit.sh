9d1d14b23ec2f15a5295d24f45a41c0de6f79af5
TeF <tefiszx@gmail.com>
2021-07-05 15:57:37 +0800

    add function for count times

--- Functions/process-monit.sh
+++ Functions/process-monit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+CHECKTIME=0
 LOGFILE="./Poweroff-time.txt"
 if [ $# -ne 1 ]; then
-    echo ">>>Please input a process name<<<";exit 1
+    echo "Please input a process name,like emerge or make etc.";exit 1
 else
     type $1 >/dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -12,10 +13,17 @@ else
         if [ ! -n "$PSCOUNT" ]; then
           echo "$1 isn't running"
 	  echo "The lastest Poweroff time is" > $LOGFILE
-	  echo $(date) >> $LOGFILE
-          /sbin/shutdown -h +3;exit 0
+	  echo $(LANG=en_US.UTF-8 date) >> $LOGFILE
+          echo "It's countdonwing 3 min to shutdown"
+	  /sbin/shutdown -h +3;exit 0
         else
-          echo ">>>$1 is running...<<<"
+        #CHECKTIME=$((CHECKTIME+1))
+        ((CHECKTIME++))
+	  if [ $CHECKTIME -ge 2 ]; then
+	    echo ">>>$1 has been running...for $CHECKTIME minutes.<<<"
+	  else
+	    echo ">>>$1 is running detected.<<<"
+	  fi
         fi
 	sleep 60
         done

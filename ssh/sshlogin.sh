2bbc49180b67a1c8ad61556c8eaeb31c48bdb605
TeF <tefiszx@gmail.com>
2021-11-23 15:09:13 +0800

    Update sshlogin.sh

--- ssh/sshlogin.sh
+++ ssh/sshlogin.sh
@@ -8,11 +8,13 @@ BLUE="34m"
 WHITE="97m"
 BLINK="\e[5m"
 
-KEYPATH="/home/tef/Vultr/id_rsa "
-VPSID1="root@173.242.112.62"
-VPSID2="ubuntu@132.226.224.151"
+KEYPATH="~/Vultr/id_rsa "
+VPSID1="root@0.1.2.3"
+VPSID2="ubuntu@1.2.3.4"
+VPSID3="root@1.2.3.4"
+ROUTER="root@192.168.1.1"
 TARGETID=""
-VPSPORT=8301
+VPSPORT=1234
 
 colorEcho(){
     echo -e "\033[${1}${@:2}\033[0m" 1>& 2
@@ -32,23 +34,23 @@ ssh -i $KEYPATH $TARGETID -p $VPSPORT
 
 
 if [ $# -ne 1 ]; then
-  colorEcho ${WHITE}"Useage: ./sshlogin.sh [1|2].   and 1 as bandwagon,2 as orcale"
+  colorEcho ${WHITE}"Useage: ./sshlogin.sh [1|2]. 1 as bandwagon,2 as orcale"
   exit 9
 fi
 case $(echo $1|tr '[:upper:]' '[:lower:]') in
 #case $1 in
-
-	1|B|b|bandwagon)
-	TARGETID=$VPSID1
-	;;
-	2|O|o|oracle)
-	TARGETID=$VPSID2
-	;;
-	*)
-	colorEcho ${YELLOW}${BLINK}"Please input vps name or number for loggin"
-	exit 9
-	;;
+        1|B|b|bandwagon)
+        TARGETID=$VPSID1
+        ;;
+        2|O|o|oracle)
+        TARGETID=$VPSID2
+        ;;
+        3|C|c|cpa)
+        TARGETID=$VPSID3
+        ;;
+        *)
+        colorEcho ${YELLOW}${BLINK}"Please input vps name or number for loggin"
+        exit 9
+        ;;
 esac
 sshloginto
-
-

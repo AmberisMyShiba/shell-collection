ff1e07f9748e30776bc9b418a06338a4ebcc7665
TeF <tefiszx@gmail.com>
2022-01-06 12:56:34 +0800

    Update glr_hysteria.sh

--- vps/glr_hysteria.sh
+++ vps/glr_hysteria.sh
@@ -13,11 +13,14 @@ colorEcho(){
 
 BIN_LOCAL_PATH="/usr/local/bin"
 BIN_FILE="hysteria"
+BIN_FILE_TUN="hysteria-tun"
 PKG_URL=$(curl -s "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" |grep hysteria-linux-amd64 | grep browser_download_url | cut -d : -f 2,3 | tr -d \")
+PKG_URL_TUN=$(curl -s "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" |grep hysteria-tun-linux-amd64 | grep browser_download_url | cut -d : -f 2,3 | tr -d \")
 PKG_LATEST=$(curl -s "https://api.github.com/repos/HyNetwork/hysteria/releases/latest" |grep hysteria-linux-amd64 | grep browser_download_url | cut -d / -f 8 |sed 's/v/-/')
 PKG_UNZIP_NAME=hysteria-linux-amd64$PKG_LATEST
+PKG_UNZIP_NAME_TUN=hysteria-tun-linux-amd64$PKG_LATEST
 #echo "PKG_UNZIP_NAME:"$PKG_UNZIP_NAME;read -p "Debug Pause"
-echo -e "1.The latest Hysteria bin-release Version is:"
+echo -e "1.The latest $BIN_NAME bin-release Version is:"
 colorEcho ${YELLOW} "   ver$PKG_LATEST"
 if [ -f "$PKG_UNZIP_NAME" ];then
   colorEcho ${RED} "2.The latest Hysteria Release file is founded in current directory."
@@ -26,8 +29,10 @@ if [ -f "$PKG_UNZIP_NAME" ];then
 fi
 echo -e "3.Downloading the latest version."
 wget $PKG_URL -q --show-progress -O $PKG_UNZIP_NAME
+wget $PKG_URL_TUN -q --show-progress -O $PKG_UNZIP_NAME_TUN
+
 echo -e "4.copy the hysteria bin file and chmod exec attribute"
-if [ ! -f "$PKG_UNZIP_NAME" ]; then
+if [ ! -f "$PKG_UNZIP_NAME" -o ! -f"$PKG_UNZIP_NAME_TUN" ]; then
   colorEcho ${YELLOW} "Not found the hysteria release bin-file,Please Check Downloading file correct!"
   exit 1
 fi
@@ -36,7 +41,9 @@ echo -e "5.stoping hysteria.service in SystemD."
 systemctl stop hysteria || kill -9 $(pidof hysteria)
 echo -e "6.Copy latest bin files to $BIN_LOCAL_PATH"
 chmod +x ./$PKG_UNZIP_NAME
+chmod +x ./$PKG_UNZIP_NAME_TUN
 cp -f $PKG_UNZIP_NAME $BIN_LOCAL_PATH/$BIN_FILE
+cp -f $PKG_UNZIP_NAME_TUN $BIN_LOCAL_PATH/$BIN_FILE_TUN
 echo -e "7.restaring hysteria.service."
 systemctl start hysteria
 colorEcho ${GREEN} "8.Cleaning Downloaded files..."

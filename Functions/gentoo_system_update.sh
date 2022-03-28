e8e0c170c945fdce254954f118e5138a92b17ba4
TeF <tefiszx@gmail.com>
2022-03-28 11:54:26 +0800

    Update gentoo_system_update.sh

--- Functions/gentoo_system_update.sh
+++ Functions/gentoo_system_update.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 intertube=0
 while [ $intertube -ne 1 ]; do
-  ping -c1 www.baidu.com > /dev/null
+  #ping -c1 www.baidu.com &> /dev/null
+  wget -T 3 -p www.google.com -o /dev/null
   if [ $? -eq 0 ]
   then
     sudo layman -S
     sudo emerge --sync
     sudo emerge -1uv portage
     sudo emerge -uDNv --with-bdeps=y --keep-going @world
-    sudo emerge -ca
+    sudo emerge -c
     intertube=1
   else 
-    echo "Waiting for Network connetcion"  && sleep 2
+    echo "Waiting for Network connetcion to GWF Outside"  && sleep 2
   fi
 done
 echo "System is updated!"

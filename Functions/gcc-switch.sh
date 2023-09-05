21575a408a83d5996c19b52a45b4027f0e3996e8
FluffyTigerfear <tefiszx@gmail.com>
2023-09-05 09:46:12 +0800

    Update gcc-switch.sh

--- Functions/gcc-switch.sh
+++ Functions/gcc-switch.sh
@@ -4,9 +4,31 @@ link_name="gcc"
 versions_dir="/usr/bin"
 link_dir="/usr/bin"
 
+gcc_rename() {
+gcc_cur=$(which gcc)
+gcc_ver=$("$gcc_cur" -dumpversion)
+if [ "$gcc_cur"="/usr/bin/gcc" ]; then
+        echo -e "\033[34m Found /usr/bin/gcc hasn't a version suffix. \033[0m"
+        local default="y"
+        read -e -p "Do you gree to rename it ?(y/n)" ac
+        ac="${ac:-${default}}"
+        case $ac in 
+                y|Y)
+                        mv "$gcc_cur" "$gcc_cur"-${gcc_ver%%.*}
+                        return 0
+                        ;;
+                n|N)
+                        echo -e "\033[32m Please rename /usr/bin/gcc to a version suffix,like /usr/bin/gcc-${gcc_ver}. \033[0m"
+                        return 1
+                        exit 1
+                        ;;
+        esac
+fi
+}
+
 list_versions() {
   echo "Available GCC versions:"
-  for file in "$versions_dir/$link_name"-*; do
+  for file in "$versions_dir/$link_name"*; do
     version=$(basename "$file" | cut -d- -f2)
     if [[ "$version" =~ ^[0-9]+$ ]]; then
       echo "- $version"
@@ -16,12 +38,15 @@ list_versions() {
 
 switch_version() {
   local version="$1"
-  local link_path="$link_dir/$link_name"
-  local target_path="$versions_dir/$link_name-$version"
+  local link_path_gcc="$link_dir/gcc"
+  local link_path_gxx="$link_dir/g++"
+  local target_path_gcc="$versions_dir/gcc-$version"
+  local target_path_gxx="$versions_dir/g++-$version"
 
-  if [ -e "$target_path" ]; then
-    sudo ln -sf "$target_path" "$link_path"
-    echo "Switched $link_name version to $version"
+  if [ -e "$target_path_gcc" ] && [ -e "$target_path_gxx" ]; then
+    sudo ln -sf "$target_path_gcc" "$link_path_gcc"
+    sudo ln -sf "$target_path_gxx" "$link_path_gxx"
+    echo "Switched gcc and g++ version to $version"
   else
     echo "Version $version not found"
     exit 1
@@ -35,10 +60,13 @@ main() {
   fi
 
   if [ $# -ne 1 ]; then
-    echo "Usage: $0 <version>"
+    echo -e "\033[32mUseage:\033[0m" 
+    echo -e "\tswitch gcc version:: $0 <version>"
+    echo -e "\tshow gcc version avalible: $0 -l"
     exit 1
   fi
 
+  gcc_rename
   switch_version "$1"
 }
 

3adb6a2ca53d8e3b32a92ae8da3c0b5c8b1d72b1
FluffyTigerfear <20527882+AmberisMyShiba@users.noreply.github.com>
2023-10-23 12:28:35 +0800

    Update sing-box-update.sh
    add choose menu for tags selecting

--- vps/sing-box-update.sh
+++ vps/sing-box-update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace with the path to the cloned repository
-REPO_PATH="github/sing-box"
+REPO_PATH="sing-box"
 
 # Move to the repository directory
 cd $REPO_PATH || { echo "Failed to change directory to $REPO_PATH"; exit 1; }
@@ -10,23 +10,44 @@ cd $REPO_PATH || { echo "Failed to change directory to $REPO_PATH"; exit 1; }
 git fetch || { echo "Failed to fetch latest changes"; exit 1; }
 
 # Get the latest tag
-TAG=$(git describe --tags $(git rev-list --tags --max-count=1)) || { echo "Failed to get latest tag"; exit 1; }
+#TAG=$(git describe --tags $(git rev-list --tags --max-count=1)) || { echo "Failed to get latest tag"; exit 1; }
+TAGS=$(git rev-list --tags --max-count=2 | xargs -I@ git describe --tags @) || { echo "Failed to get latest tag"; exit 1; }
 
-# Checkout the latest tag
-git checkout $TAG || { echo "Failed to checkout latest tag: $TAG"; exit 1; }
+# 列出TAGS的值，每行显示一个tag，并在每个tag前加入数字编号echo "Available tags:"
+i=1
+while IFS= read -r tag; do
+    echo "$i. $tag"
+    tags_array+=("$tag")
+    i=$((i+1))
+done <<< "$TAGS"
+
+# 询问切换到哪一个tag
+read -p "Which tag (by number or full name) do you want to checkout? " selected_tag
+
+# 检查选择的是编号还是全文，并设置正确的标签值if [[ "$selected_tag" =~ ^[0-9]+$ ]] && [ "$selected_tag" -le ${#tags_array[@]} ]; then
+    selected_tag=${tags_array[$selected_tag-1]}
+fi
+
+# 使用git checkout 切换到选择的那个tag
+if echo "$TAGS" | grep -q "^$selected_tag$"; then
+    git checkout "$selected_tag"
+    echo "Switched to tag $selected_tag"
+else
+    echo "Tag $selected_tag not found in the list."
+fi
 
 # Print the checkout message
-echo "Checked out the latest tag: $TAG"
+echo "Checked out the latest tag: $selected_tag"
 
 # Ask the user if they want to continue compiling
-read -p "Do you want to continue compiling? [y/n] " choice
-case "$choice" in 
-  y|Y ) 
+read -rp "Do you want to continue compiling? [y/n] " choice
+case "${choice,,}" in
+  y|"" )
     # Run the release/local/reintall.sh script
     ./release/local/reinstall.sh
-    sing-box version
+        sing-box version
     ;;
-  * ) 
+  * )
     echo "Aborted."
     ;;
 esac

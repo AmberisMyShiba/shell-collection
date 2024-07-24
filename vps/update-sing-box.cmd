2ef07b64bc032895aa03830f91ca1ef434c42640
FluffyTigerfear <20527882+AmberisMyShiba@users.noreply.github.com>
2024-07-24 18:25:27 +0800

    Update update-sing-box.cmd

--- vps/update-sing-box.cmd
+++ vps/update-sing-box.cmd
@@ -66,7 +66,12 @@ if /i "%choice%"=="y" (
     set VERSION=%%c
   )
   echo sing-box VERSION is !VERSION!
-  go install -v -trimpath -ldflags "-X \"github.com/sagernet/sing-box/constant.Version=$VERSION\"-s -w -buildid=" -tags with_gvisor,with_clash_api,with_grpc,with_utls,with_ech,with_reality_server,with_quic,with_wireguard,with_acme .\cmd\sing-box
+  go install -v -trimpath -ldflags "-X \"github.com/sagernet/sing-box/constant.Version=!VERSION!\"-s -w -buildid=" -tags with_gvisor,with_clash_api,with_grpc,with_utls,with_ech,with_reality_server,with_quic,with_wireguard,with_acme .\cmd\sing-box
+
+
+
+
+
   %GOBIN%\sing-box version
   cd ..\
 ) else (
